ef21a0c25e5434595f33e020b6f6bed8c4c0c0a9
Simone Pucci <simopucci2002@gmail.com>
2020-11-26 15:34:46 +0100

    sviluppata gestione dinamica request

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8000;
 
 const handlebars = require('express-handlebars');
 
@@ -18,8 +18,35 @@ partialsDir: __dirname + '/views/partials/'
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-//Using the index.hbs file instead of planB
-res.render('main', {layout: 'index'});
+let i=req.headers['user-agent']
+if(i.search('Edg')<0 && (req.query.name==="Simone"||req.query.id==="27"||req.query.sur==="Pucci")){
+	res.render('main', {layout: 'index', greeting: 'Hello World!', jumbo:"Hi master"})
+}
+if(i.search('Edg')<0 && (req.query.name!="Simone" && req.query.id!="27" && req.query.sur!="Pucci")){
+	res.render('main', {layout: 'index', greeting: 'Hello World!', jumbo:"This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."})
+}
+else{
+	res.render('main', {layout: 'index', greeting: 'stai usando Edge, mona!'})
+}
+
+});
+
+app.get('/learn', (req, res) => {
+let i=req.headers['user-agent']
+if(i.search('Edg')<0 && (req.query.name==="Simone"||req.query.id==="27"||req.query.sur==="Pucci")){
+	res.render('learn', {layout: 'index', typeAl: 'alert alert-success', content: 'task completed master'})
+}
+if(i.search('Edg')<0 && (req.query.name!="Simone" && req.query.id!="27" && req.query.sur!="Pucci")){
+	res.render('learn', {layout: 'index', typeAl: 'alert alert-success', content: 'task completed'})
+}
+else{
+	res.render('learn', {layout: 'index', typeAl: 'alert alert-danger', content: 'mission failed'})
+}
+
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
+
+/*"npm i --save-dev @soluzioni-futura/eslint-config-soluzioni-futura" {
+  "extends": "@soluzioni-futura/eslint-config-soluzioni-futura"
+}*/
